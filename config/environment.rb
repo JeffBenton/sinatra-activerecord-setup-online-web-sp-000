aa6e5dde9fb188a4eb0efd5e1b63b70772a07677
Jeff Benton <sikletz@gmail.com>
2019-05-10 19:22:27 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -15,8 +15,6 @@ require 'active_record'
 
 Bundler.require
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
 DB = ActiveRecord::Base.establish_connection({
   adapter: 'sqlite3',
